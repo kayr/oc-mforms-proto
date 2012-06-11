1838dba9ea79425a0f142e1af26f655b681d66d8
kayr <noryak1234@gmail.com>
2012-06-11 18:37:26 +0400

    Adding the subject eventOID to the XML

--- src/main/java/org/openxdata/workflow/proto/handler/WIRUpload.java
+++ src/main/java/org/openxdata/workflow/proto/handler/WIRUpload.java
@@ -1,14 +1,29 @@
 package org.openxdata.workflow.proto.handler;
 
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import java.util.logging.Level;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.apache.log4j.Logger;
 import org.openxdata.mforms.server.XFormSerializer;
 import org.openxdata.mforms.model.ResponseHeader;
@@ -20,6 +35,11 @@ import org.openxdata.proto.WFSubmissionContext;
 import org.openxdata.proto.exception.ProtocolException;
 import org.openxdata.workflow.mobile.model.MWorkItemData;
 import org.openxdata.workflow.mobile.model.MWorkItemDataList;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -65,7 +85,9 @@ public class WIRUpload implements RequestHandler {
         log.debug("upload for Workitem: [" + mWirData.getCaseId() + "] contains:  forms=" + allStudies.getStudies().size());
 
         List<String> deserialise = deserialise(allStudies, xForms);
-        context.setUploadResult(deserialise);
+	    for (String string : deserialise) {
+		    context.setUploadResult(processXML(mWirData.getCaseId(), string));
+	    }
         writeUploadResponse(out);
 
     }
@@ -106,4 +128,82 @@ public class WIRUpload implements RequestHandler {
         }
         return xmlForms;
     }
+    
+	String processXML(String workitemID, String xml) {
+		try {
+			String eventID = extractEventOID(workitemID);
+			String newXML = updateXMLWithEventAttribute(eventID, xml);
+			return newXML;
+		} catch (Exception ex) {
+			log.error("Error while update the workitem XML", ex);
+		}
+		return xml;
+	}
+    
+    
+	String extractEventOID(String workItemID) {
+		String result = "UNKNOWN";
+		if (workItemID == null) {
+			return result;
+		}
+		int begin = workItemID.indexOf('&');
+		if (begin == -1) {
+			return result;
+		}
+		workItemID = workItemID.trim();
+		
+		try {
+			result = workItemID.substring(begin + 1);
+			if(result.isEmpty())
+				result = "UNKNOWN";
+		} catch (Exception e) {
+			return result;
+		}
+		return result;
+	}
+	
+	private String updateXMLWithEventAttribute(String eventOID, String xml) throws ParserConfigurationException, SAXException, IOException, TransformerConfigurationException, TransformerException {
+		Document doc = getDocument(xml);
+		Node firstChild = doc.getFirstChild();
+		NamedNodeMap attributes = firstChild.getAttributes();
+		Attr studyEventAttr = doc.createAttribute("StudyEventOID");
+		studyEventAttr.setValue(eventOID);
+		attributes.setNamedItem(studyEventAttr);
+		String resultXML = fromDocToString(doc);
+		return resultXML;
+	}
+	
+	private String fromDocToString(Document doc) throws TransformerConfigurationException, TransformerException{
+		Transformer transformer = getTranformer();
+		
+		StreamResult result = new StreamResult(new StringWriter());
+		DOMSource source = new DOMSource(doc);
+		transformer.transform(source, result);
+		String xml = result.getWriter().toString();
+		return xml;
+	}
+	
+	private Transformer mTranformer;
+
+	private Transformer getTranformer() throws IllegalArgumentException, TransformerConfigurationException, TransformerFactoryConfigurationError {
+		if (mTranformer == null) {
+			mTranformer = TransformerFactory.newInstance().newTransformer();
+			mTranformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		}
+		return mTranformer;
+	}
+	
+	private DocumentBuilder mDocBuilder;
+
+	private Document getDocument(String xml) throws SAXException, IOException, ParserConfigurationException {
+		if (mDocBuilder == null) {
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			mDocBuilder = docFactory.newDocumentBuilder();
+
+		}
+		Document doc = mDocBuilder.parse(new ByteArrayInputStream(xml.getBytes()));
+		return doc;
+	}
+	
+	
 }
---
+++ src/test/java/org/openxdata/workflow/proto/handler/WIRUploadTest.java
@@ -0,0 +1,60 @@
+package org.openxdata.workflow.proto.handler;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openxdata.proto.WFSubmissionContext;
+
+/**
+ *
+ * @author kay
+ */
+public class WIRUploadTest {
+
+	public WIRUploadTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	@Test
+	public void extractEventOID() throws Exception {
+		WIRUpload instance = new WIRUpload();
+		String result = instance.extractEventOID(null);
+		assertEquals("UNKNOWN", result);
+
+		result = instance.extractEventOID("BLAHEH");
+		assertEquals("UNKNOWN", result);
+
+		result = instance.extractEventOID("SHSHSH&SC3");
+		assertEquals("SC3", result);
+
+		result = instance.extractEventOID("SHSHSH&");
+		assertEquals("UNKNOWN", result);
+	}
+
+	@Test
+	public void processXML() throws Exception {
+		WIRUpload instance = new WIRUpload();
+		String xml = "<?xml version=\"1.0\" ?>"
+			+ "<earth>"
+			+ "    <country>us</country>"
+			+ "</earth>";
+		String expectedResult = "<?xmlversion=\"1.0\"encoding=\"UTF-8\"standalone=\"no\"?>"
+			+ "<earth StudyEventOID=\"SC3\">"
+			+ "    <country>us</country>"
+			+ "</earth>";
+		expectedResult = expectedResult.replaceAll("\\s+", "");
+
+		String result = instance.processXML("S0053&SC3", xml);
+		result = result.replaceAll("\\s+", "");
+		assertEquals(expectedResult, result);
+
+	}
+}
